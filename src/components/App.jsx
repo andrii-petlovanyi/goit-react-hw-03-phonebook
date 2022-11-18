2d7efa10d11355f48354d6d32e2ac5a586725dfa
Andrii Petlovanyi <33178699+andrii-petlovanyi@users.noreply.github.com>
2022-11-18 16:04:46 +0200

    completed task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,14 +12,22 @@ import {
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const contacts = JSON.parse(localStorage.getItem('KEY_CONTACTS'));
+    if (contacts) {
+      this.setState({ contacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('KEY_CONTACTS', JSON.stringify(this.state.contacts));
+    }
+  }
 
   addContact = contact => {
     if (
@@ -112,13 +120,15 @@ export class App extends Component {
           fontFamily={theme.fonts.title}
           justifyContent="center"
         >
-          Contacts
+          {this.state.contacts.length > 0 ? 'Contacts' : 'No contacts'}
         </Box>
-        <FilterContacts
-          onChange={this.onChange}
-          value={this.state.filter}
-          onFilter={this.onFilter}
-        />
+        {this.state.contacts.length > 1 && (
+          <FilterContacts
+            onChange={this.onChange}
+            value={this.state.filter}
+            onFilter={this.onFilter}
+          />
+        )}
         <ContactList
           contacts={this.state.contacts}
           filter={this.state.filter}
